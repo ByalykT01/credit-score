4348d15d85da1efdc7ab8c6f8be461fc4567fe1d
Taras Byalyk <byalykt@hotmail.com>
2024-07-09 17:42:40 +0200

    navbar and pre-signup page basic design

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,17 +1,17 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-export function TopNav(){
+export function TopNav() {
   return (
-    <nav className="flex w-full items-center justify-between p-5 font-semibold border-b text-3xl">
-        <div>Credit</div>
-        <div>
-          <SignedOut>
-            <SignInButton/>
-          </SignedOut>
-          <SignedIn>
-            <UserButton/>
-          </SignedIn>
-        </div>
+    <nav className="flex w-full items-center justify-between border-b bg-black p-5 text-3xl font-semibold">
+      <div>Credit</div>
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </nav>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,34 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
+import { WelcomePage } from "./_components/welcomepage";
 
 export const dynamic = "force-dynamic";
 interface User {
-  name: string
-  amount: number
-  bank: string
-};
+  name: string;
+  amount: number;
+  bank: string;
+}
 
 const user1: User = {
   name: "user1",
-  amount: 50.00,
-  bank: "Pekao"
+  amount: 50.0,
+  bank: "Pekao",
 };
 
 const user2: User = {
   name: "user2",
-  amount: 150.00,
-  bank: "Pekao"
+  amount: 150.0,
+  bank: "Pekao",
 };
 const user3: User = {
   name: "user3",
   amount: 0,
-  bank: "Barclays"
+  bank: "Barclays",
 };
 const user4: User = {
   name: "user4",
   amount: 69.42,
-  bank: "Santander"
+  bank: "Santander",
 };
 
 const users = [user1, user2, user3, user4];
@@ -34,33 +36,36 @@ const users = [user1, user2, user3, user4];
 const mockUsers = users.map((user, index) => ({
   id: index + 1,
   user,
-}))
+}));
 
 export default async function HomePage() {
   const posts = await db.query.posts.findMany();
-  console.log(posts)
+  console.log(posts);
 
   return (
     <main>
-      <div className='flex flex-wrap gap-4'>
-        {mockUsers.map((user) => (
-          <div key={user.id} className="w-48">
-            <h1>{user.user.name}</h1>
-            <h2>Bank: {user.user.bank}</h2>
-            <h2>{user.user.amount} PLN</h2>
+      <SignedOut>
+        <WelcomePage />
+      </SignedOut>
+      <SignedIn>
+        <div className="flex flex-wrap gap-4 ">
+          {mockUsers.map((user) => (
+            <div key={user.id} className="w-48">
+              <h1>{user.user.name}</h1>
+              <h2>Bank: {user.user.bank}</h2>
+              <h2>{user.user.amount} PLN</h2>
+            </div>
+          ))}
+        </div>
+        <div className="grid h-56 items-center justify-center gap-4">
+          <div className="text-2xl font-semibold ">GIVE ME YOUR DATA</div>
+          <div className="grid gap-10 text-2xl font-semibold">
+            {posts.map((post) => (
+              <div key={post.id}>{post.name}</div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="grid justify-center items-center h-56 gap-4">
-        <div className="text-2xl font-semibold ">
-          GIVE ME YOUR DATA
         </div>
-        <div className="text-2xl font-semibold grid gap-10">
-           {posts.map((post) => (
-            <div key={post.id}>{post.name}</div>
-           ))}
-        </div>
-      </div>
+      </SignedIn>
     </main>
   );
 }
